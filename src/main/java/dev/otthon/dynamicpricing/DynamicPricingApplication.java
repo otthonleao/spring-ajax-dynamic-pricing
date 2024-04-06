b8c7f1137a15b64cd55182967c3f69d416dda2df
otthonleao <otthonleao@hotmail.com>
2024-04-06 01:20:29 -0400

    build: initial commit

---
+++ src/main/java/dev/otthon/dynamicpricing/DynamicPricingApplication.java
@@ -0,0 +1,13 @@
+package dev.otthon.dynamicpricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicPricingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicPricingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/otthon/dynamicpricing/DynamicPricingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otthon.dynamicpricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicPricingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
