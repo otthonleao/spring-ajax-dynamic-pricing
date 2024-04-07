bb234bc234d2ad53dc66bb8b14587dc670eb905f
otthonleao <otthonleao@hotmail.com>
2024-04-07 17:24:01 -0400

    feat: add `Categoria.java` class in domain

---
+++ src/main/java/dev/otthon/dynamicpricing/domain/Categoria.java
@@ -0,0 +1,66 @@
+package dev.otthon.dynamicpricing.domain;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+@SuppressWarnings("serial")
+public class Categoria implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="titulo", nullable = false, unique = true)
+    private String titulo;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Promocao> promocoes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public List<Promocao> getPromocoes() {
+        return promocoes;
+    }
+
+    public void setPromocoes(List<Promocao> promocoes) {
+        this.promocoes = promocoes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id) && Objects.equals(titulo, categoria.titulo) && Objects.equals(promocoes, categoria.promocoes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, titulo, promocoes);
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                '}';
+    }
+}
