255e4f8d365579a64b41c1ce2ca746f8efc4972b
otthonleao <otthonleao@hotmail.com>
2024-04-07 01:26:21 -0400

    feat: take the error 404 and 500 to show alert

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -10,6 +10,14 @@ $(" #linkPromocao ").on('change', function() {
                 $("#titulo").val(data.title);
                 $("#site").text(data.site.replace("@", ""));
                 $("#linkImagem").attr("src", data.image);
+            },
+            statusCode: {
+                404: function () {
+                    $("#alert").addClass("alert alert-danger").text("Nenuma informação pode ser recuperada desta URL")
+                },
+                error: function () {
+                    $("#alert").addClass("alert alert-danger").text("Erro 500: Algo deu errado, tente novamente mais tarde")
+                }
             }
         })
     }
