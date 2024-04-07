4f00dcf600fa8ec2e1381b54da4d228f1809f9b2
otthonleao <otthonleao@hotmail.com>
2024-04-07 02:45:10 -0400

    feat: add loader css on the picture

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -9,7 +9,8 @@ $(" #linkPromocao ").on('change', function() {
                 $("#alert").removeClass("alert alert-danger").text("");
                 $("#titulo").val("");
                 $("#site").text("");
-                $("#linkImagem").attr("src", "./images/promo-dark.png");
+                $("#linkImagem").attr("src", "");
+                $("#loader-img").addClass("loader");
             },
             success: function (data) {
                 console.log(data);
@@ -20,10 +21,15 @@ $(" #linkPromocao ").on('change', function() {
             statusCode: {
                 404: function () {
                     $("#alert").addClass("alert alert-danger").text("Nenuma informação pode ser recuperada desta URL")
+                    $("#linkImagem").attr("src", "/images/promo-dark.png");
                 },
                 error: function () {
                     $("#alert").addClass("alert alert-danger").text("Erro 500: Algo deu errado, tente novamente mais tarde")
+                    $("#linkImagem").attr("src", "/images/promo-dark.png");
                 }
+            },
+            complete: function () {
+                $("#loader-img").removeClass("loader");
             }
         })
     }
