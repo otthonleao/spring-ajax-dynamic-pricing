ae18609b99d1c1de1928b6c43cb32ba583009190
otthonleao <otthonleao@hotmail.com>
2024-04-11 19:32:15 -0400

    build: add H2 Database and script SQL with 'categorias'

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,8 @@
+-- Inserção de dados na tabela categorias
+INSERT INTO categoria VALUES (1,'Informatica'),
+                             (2,'Cursos'),
+                             (3,'Eletrodomésticos'),
+                             (4,'Eletronicos'),
+                             (5,'Livros'),
+                             (6,'Móveis'),
+                             (7,'Cama, Mesa e Banho');
