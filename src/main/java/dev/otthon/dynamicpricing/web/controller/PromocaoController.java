15d7aea583aa5bd4eb178bd6ddbac26d4d93714e
otthonleao <otthonleao@hotmail.com>
2024-04-12 00:13:21 -0400

    chore: add bean validation `@Valid` and Errors can be when save as status code 422.

--- src/main/java/dev/otthon/dynamicpricing/web/controller/PromocaoController.java
+++ src/main/java/dev/otthon/dynamicpricing/web/controller/PromocaoController.java
@@ -4,16 +4,21 @@ import dev.otthon.dynamicpricing.domain.Categoria;
 import dev.otthon.dynamicpricing.domain.Promocao;
 import dev.otthon.dynamicpricing.repository.CategoriaRepository;
 import dev.otthon.dynamicpricing.repository.PromocaoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @Controller
@@ -28,7 +33,17 @@ public class PromocaoController {
     private CategoriaRepository categoriaRepository;
 
     @PostMapping("/save")
-    public ResponseEntity<Promocao> salvarPromocao(Promocao promocao) {
+    public ResponseEntity<?> salvarPromocao(@Valid Promocao promocao, BindingResult result) {
+
+        if (result.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError error : result.getFieldErrors()) {
+                errors.put(error.getField(), error.getDefaultMessage());
+            }
+            // Retornando Status Code 422
+            return ResponseEntity.unprocessableEntity().body(errors);
+        }
+
         log.info("Promocao {}");
         promocao.setDtCadastro(LocalDateTime.now());
         promocaoRepository.save(promocao);
